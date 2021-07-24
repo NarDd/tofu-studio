e2fbbfdfdccad2ec92fb19438e1f878d6784ca70
NarDd <bernardng.kf@gmail.com>
2021-07-25 04:59:00 +0800

    fix: Test fix for newly added sass file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
 import Construction from '../components/construction'
+import "../styles/index.sass";
 // import Masonry from 'react-masonry-component'
 // import Img from 'gatsby-image'
 // import Layout from "../components/layout"
